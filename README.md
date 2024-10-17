9ccf9a3642312c5a1e84742ab47acccb150ffd9e
Shubham Chaturvedi <shubhamchat224122@gmail.com>
2024-10-17 21:08:42 +0530

    added blog site what i used

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 
 npm run ssr   #for SSR
 npm start     #for CSR
+
+
+
+
+https://medium.com/simform-engineering/how-to-implement-ssr-server-side-rendering-in-react-18-e49bc43e9531
--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-// server/index.js
-require("ignore-styles");
 
-require("@babel/register")({
+require('ignore-styles')
+
+require('@babel/register')({
   ignore: [/(node_modules)/],
-  presets: ["@babel/preset-env", "@babel/preset-react"],
-});
+  presets: ['@babel/preset-env', '@babel/preset-react']
+})
 
-require("./server");
+require('./server')
